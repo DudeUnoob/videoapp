b2c00111c42bd8ebcff8ef7ab1f988a7b6b32470
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2024-04-14 22:03:21 -0500

    Update usersVideo.js

--- models/usersVideo.js
+++ models/usersVideo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect("mongodb+srv://ZeroX:Balaram26@cluster0.b2lzi.mongodb.net/Roast-API?retryWrites=true&w=majority").then(() => console.log(`Connected to db`))
+mongoose.connect("").then(() => console.log(`Connected to db`))
 
 let Schema = new mongoose.Schema({
     videoId: String,
